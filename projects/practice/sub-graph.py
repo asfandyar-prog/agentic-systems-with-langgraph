da2e05cd6f34a95668bb6eb7efeff3e54ae3d25e
Asfand Yar <asfandyar@mailbox.unideb.hu>
2026-03-11 11:56:56 +0100

    practice subgraph for multi-agent sustem

--- projects/practice/sub-graph.py
+++ projects/practice/sub-graph.py
@@ -1,37 +1,37 @@
-from typing import TypedDict
-from langgraph.graph.state import StateGraph, START, END
+# from typing import TypedDict
+# from langgraph.graph.state import StateGraph, START, END
 
 
-class SubgraphState(TypedDict):
-    bar:str
-    #SubgraphState can have any fields, but for this example we just have one field called "bar"
+# class SubgraphState(TypedDict):
+#     bar:str
+#     #SubgraphState can have any fields, but for this example we just have one field called "bar"
 
-def mock_subgraph(state:SubgraphState):
-    return {"bar":"hello world" +state["bar"]}
-
-
-subgraph_builder=StateGraph(SubgraphState)
-subgraph_builder.add_node(mock_subgraph)
-subgraph_builder.add_edge(START,"mock_subgraph")
-subgraph=subgraph_builder.compile()
+# def mock_subgraph(state:SubgraphState):
+#     return {"bar":"hello world" +state["bar"]}
 
 
+# subgraph_builder=StateGraph(SubgraphState)
+# subgraph_builder.add_node(mock_subgraph)
+# subgraph_builder.add_edge(START,"mock_subgraph")
+# subgraph=subgraph_builder.compile()
 
-#Parent Graph
 
-class State(TypedDict):
-    foo:str
+
+# #Parent Graph
+
+# class State(TypedDict):
+#     foo:str
     
-def call_subgraph(state:State):
-    #Transform the state to the subgraph state
-    subgraph_output=subgraph.invoke({"bar":state["foo"]})
-    #Transform the subgraph output back to the parent graph state
-    return {"foo":subgraph_output["bar"]}
+# def call_subgraph(state:State):
+#     #Transform the state to the subgraph state
+#     subgraph_output=subgraph.invoke({"bar":state["foo"]})
+#     #Transform the subgraph output back to the parent graph state
+#     return {"foo":subgraph_output["bar"]}
 
-graph_builder= StateGraph(State)
-graph_builder.add_node("node-1",call_subgraph)
-graph_builder.add_edge(START,"node-1")
-graph=graph_builder.compile()
+# graph_builder= StateGraph(State)
+# graph_builder.add_node("node-1",call_subgraph)
+# graph_builder.add_edge(START,"node-1")
+# graph=graph_builder.compile()
 
 
 # example with different State schemas
@@ -57,3 +57,37 @@ subgraph_builder.add_edge(START,"node-1")
 subgraph_builder.add_edge("node-1","node-2")    
 subgraph_builder.add_edge("node-2",END)
 subgraph=subgraph_builder.compile() 
+
+class ParentGraphState(TypedDict):
+    foo:str
+def node_1(state:ParentGraphState):
+    return {"foo":"hi!"+state["foo"]}
+def node_2(state:ParentGraphState):
+    #Transform the state to the subgraph state
+    response =  subgraph.invoke({"bar":state["foo"]})
+    #Transform the subgraph output back to the parent graph state
+    return {"foo":response["bar"]}
+
+
+
+parent_graph_builder=StateGraph(ParentGraphState)
+parent_graph_builder.add_node("node-1",node_1)
+parent_graph_builder.add_node("node-2",node_2)  
+parent_graph_builder.add_edge(START,"node-1")
+parent_graph_builder.add_edge("node-1","node-2")
+
+graph=parent_graph_builder.compile()
+
+# for chunk in parent_graph.stream({"foo":"foo"},subgraphs=True,version="v2"):
+#     if chunk["type"]=="updates":
+#         print(chunk["ns"],chunk["data"])
+
+# for chunk in graph.stream({"foo": "foo"}, subgraphs=True, version="v2"):
+#     if int(chunk["type"]) ==  int("updates"):
+#         print(chunk["ns"], chunk["data"])
+
+for ns, chunk in graph.stream({"foo": "foo"}, subgraphs=True, version="v2"):
+    if ns == ():                    # only parent graph updates
+        print("Parent graph:", chunk)
+    else:
+        print("   Subgraph →", ns, chunk)
